f564c7eed4c9ddd485d907fc7fa81ffe9043345c
Sherman Lim <shermz.lim.jh@gmail.com>
2019-03-02 21:54:14 +0800

    Added flipcoin plots simulation

---
+++ experiment_codes/flipPlot.py
@@ -0,0 +1,35 @@
+import random
+import pylab
+
+def flipPlot(minExp, maxExp):
+    """Assumes minExp and maxExp positive integers; minExp < maxExp
+    Plots results of 2**minExp to 2**maxExp coin flips"""
+    ratios = []
+    diffs = []
+    xAxis = []
+    for exp in range(minExp, maxExp + 1):
+        xAxis.append(2**exp)
+    for numFlips in xAxis:
+        numHeads = 0
+        for n in range(numFlips):
+            if random.random() < 0.5:
+                numHeads += 1
+        numTails = numFlips - numHeads
+        ratios.append(numHeads/float(numTails))
+        diffs.append(abs(numHeads - numTails))
+    pylab.title('Difference Between Heads and Tails')
+    pylab.xlabel('Number of Flips')
+    pylab.ylabel('Abs(#Heads - #Tails)')
+    pylab.semilogx()
+    pylab.semilogy()
+    pylab.plot(xAxis, diffs, 'bo')
+    pylab.figure()
+    pylab.semilogx()
+    pylab.semilogy()
+    pylab.title('Heads/Tails Ratios')
+    pylab.xlabel('Number of Flips')
+    pylab.ylabel('#Heads/#Tails')
+    pylab.plot(xAxis, ratios, 'bo')
+random.seed(0)
+flipPlot(4, 20)
+pylab.show()
