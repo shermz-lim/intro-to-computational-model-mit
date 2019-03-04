89d05541f41f2837cb64fce0335a7426a6d16e42
Sherman Lim <shermz.lim.jh@gmail.com>
2019-03-04 20:59:35 +0800

    small modifications

--- PS1/ps1b.py
+++ PS1/ps1b.py
@@ -23,32 +23,36 @@ def dp_make_weight(egg_weights, target_weight, memo = {}):
     
     Returns: int, smallest number of eggs needed to make target weight
     """
-    
-    # base case
+    # solution: recursion + memoization. Recursion problem to solve: minimum no. of eggs needed to fulfil target weight 
+
+    # base case: only 1 egg needed when 1 of the egg can fulfil the target weight 
     if target_weight in egg_weights:
         return 1
 
+    # initialize smallest number of eggs for this node as infinity 
+    # if no branch can be entered, node will need infinity no. of eggs 
     smallest_num_eggs = float('inf')
 
-    # recursive case: 1 branch for each diff. egg weights 
+    # recursive case: enters 1 branch for each diff. egg weights 
     for weight in egg_weights:
         remaining_weight = target_weight - weight 
-        print(remaining_weight)
 
-        # only enter branch if remaining weight can be fulfilled by egg weights 
         if remaining_weight > 0:
-            
             try:
                 # checks whether recursive solution is already in memo 
                 recursive_num_eggs = memo[remaining_weight]
             except KeyError:
-                recursive_num_eggs = dp_make_weight(egg_weights, remaining_weight, memo)  
-                memo[target_weight] = (recursive_num_eggs + 1)
+                # node's possible no. of eggs = 1 + min no. of eggs in branch node 
+                recursive_num_eggs = dp_make_weight(egg_weights, remaining_weight, memo) + 1
+                # stores solution in memo for future use 
+                memo[remaining_weight] = recursive_num_eggs
 
             if recursive_num_eggs < smallest_num_eggs:
+                # keeps track of the smallest no. of eggs 
                 smallest_num_eggs = recursive_num_eggs    
 
-    return (smallest_num_eggs + 1)
+    # return min no. of eggs out of all possible ones 
+    return smallest_num_eggs
 
 
 
@@ -56,7 +60,7 @@ def dp_make_weight(egg_weights, target_weight, memo = {}):
 
 # EXAMPLE TESTING CODE, feel free to add more if you'd like
 if __name__ == '__main__':
-    egg_weights = (1, 5, 10, 25)
+    egg_weights = (1, 5, 10, 20)
     n = 99
     print("Egg weights = (1, 5, 10, 25)")
     print("n = 99")
